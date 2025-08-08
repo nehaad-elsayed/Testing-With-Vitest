f2f1535d97f0e142036c71d0edcb417bafbc4085
Nehad <nehad.elsayed@robustastudio.com>
2025-08-08 22:27:07 +0300

    fix vitest.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: "happy-dom",
-    setupFiles: ["./src/setupTests.ts"],
-    globals: true,
-  },
 });
